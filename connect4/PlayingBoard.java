78a714b5a71d5f11dca5e8b06e03b34e2f17b86c
sergeipwnu <serioshalevashov@yahoo.com>
2017-05-08 16:25:53 -0700

    areFourConnected() check added to makeMove()

--- connect4/PlayingBoard.java
+++ connect4/PlayingBoard.java
@@ -115,6 +115,11 @@ public class PlayingBoard
             }
             System.out.println();
         }
+        if(areFourConnected())
+        {
+            changePlayer();
+            System.out.println(">>>>>>>PLAYER " + playerNum + " WINS<<<<<<<<<");
+        }
         return b;
     }
     
