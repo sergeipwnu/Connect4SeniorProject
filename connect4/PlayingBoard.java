4c721786c66fbd080c2760b5c750a87c6f59e239
omer6100 <omerhananya@Omers-MacBook-Air.local>
2017-05-08 12:18:02 -0700

    MakeMove done

--- connect4/PlayingBoard.java
+++ connect4/PlayingBoard.java
@@ -10,10 +10,9 @@ public class PlayingBoard
     private final int ROW = 6;
     private final int COL = 7;
     private boolean connect4 = false;
-<<<<<<< HEAD
+
     private int playerNum = 1;
-=======
->>>>>>> origin/master
+
     private int[][] moves;
 
     /**
@@ -21,47 +20,52 @@ public class PlayingBoard
      */
     public PlayingBoard()
     {
-<<<<<<< HEAD
-        moves = new int[ROW][COL];
-=======
         moves = new int[ROW][COL]; //0 if no piece, 1 if player1, 2 if player2.
->>>>>>> origin/master
     }
     
     /**
-     * 
+     * OMER
      */
     public boolean areFourConnected()
     {
-        //this will be checked and update the variable connect4 after every time makeMove is successful
-        return true;
+        //this will be checked and update the variable connect4 
+        //after every time makeMove is successful
+        for (int i = 0; i < 
+        
+        //vertical
+        //horizontal
+        //diagonal
     }
     
     /**
      * TO BE TESTED WHEN GAMEPLAY IMPLEMENTED
+     * OMER
      */
     public boolean makeMove(Position p) 
     {
         System.out.println(p);
         boolean b;
+        
         if(moves[p.y][p.x] > 0)
         {
             b = false;
-            System.out.println("hit nest");
+            System.out.println("cant do that buddy");
         }
-        else
+        else 
         {
-            int below = p.y;
             
-            while(below+1 <= ROW && moves[below][p.x] == 0)
-                below++;
+            int placeBelow = p.y;
+            while(placeBelow+1 < ROW && moves[placeBelow+1][p.x] == 0)
+            {
+                placeBelow++;
+            }
             
-            moves[below][p.x] = playerNum;
+            moves[placeBelow][p.x] = playerNum;
             changePlayer();
-            System.out.println("hit nest");
             b = true;
         }
-        System.out.println("hit nest");
+        
+        System.out.println("board");
         for(int i =0; i < ROW; i++)
         {
              for(int j = 0; j<COL; j++)
@@ -69,8 +73,7 @@ public class PlayingBoard
                 System.out.print(moves[i][j]);
             }
             System.out.println();
-        } 
-        
+        }
         return b;
     }
     
