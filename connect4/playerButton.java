8bfc324239892c1f591510844288711ef69dc107
sergeipwnu <serioshalevashov@yahoo.com>
2017-05-12 19:18:31 -0700

    added audio support for menu, fixed jFX errors with toolkit initialization

--- connect4/playerButton.java
+++ connect4/playerButton.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import java.awt.event.*;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.util.Duration;
+import java.io.File;
 /**
  * A modified JButton class with left and top insets, as well as a name and imagepath
  * 
@@ -9,6 +13,7 @@ public class playerButton extends JButton
 {
     private String myName;
     private Menu myMenu;
+    private MediaPlayer pick;
     /**
      * adds a listener, assigns local variables, and sets bounds.
      * 
@@ -16,15 +21,24 @@ public class playerButton extends JButton
      */
     playerButton(int left, int top, String name, Menu m)
     {
+        
         addListener();
         myMenu = m;
         myName = name;
         setBounds(left,top,150,150);
         setText(name);
+        pick = new MediaPlayer(new Media(new File("audio/" + "omid" + "name.wav").toURI().toString())); //THIS PLAYS ONLY OMIDS VOICE LINE FOR TESTING PURPOSES
     }
     public Player getPlayer()
     {
         return new Player(myName);
+    }
+    private void playPick()
+    {
+        System.out.println("play pick audio file for " + myName);
+        pick.play();
+        pick.seek(Duration.ZERO);
+        
     }
     /**
      * adds a listener for the playerButton click.
@@ -40,10 +54,12 @@ public class playerButton extends JButton
                 if(myMenu.isPicked())
                 {
                     myMenu.setPlayer2(getPlayer());
+                    playPick();
                     myMenu.initBoard();
                 }
                 else
                 {
+                    playPick();
                     myMenu.setPlayer1(getPlayer());
                     myMenu.setPicked();
                 }
